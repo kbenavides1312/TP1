6ed49625f77e9b0a87df3b9ee0756b81cfc30409
Kenneth Benavides <kbr1312@gmail.com>
2020-09-27 14:25:48 -0600

    Crear Constructor Arbitro & Implementar Tablero.trasvasarBola

--- Tarea_Programada_1/Arbitro.java
+++ Tarea_Programada_1/Arbitro.java
@@ -3,18 +3,31 @@ public class Arbitro
 {   
     private int cantColores;
     private int tamanoTubo;
+    private Computadora computadora;
     private Tablero tablero;
     private int cantidadMovimientos;
 
-    public Arbitro(int cantColores, int tamanoTubo){
+    public Arbitro(int cantColores, int tamanoTubo, int cantTubosVacios){
         this.cantColores = cantColores;
         this.tamanoTubo = tamanoTubo;
-        this.tablero = new Tablero();
+        this.computadora = new Computadora(cantColores,tamanoTubo);
+        this.tablero = new Tablero(cantColores, tamanoTubo, computadora.generarDistribucion(), cantTubosVacios);
     }
-    public void trasvasarBola(){
+    
+    public boolean trasvasarBola(int tuboSalida, int tuboEntrada){
+        int bola;
+        bola = tablero.quitarBola(tuboSalida);
+        if (tablero.agregarBola(tuboEntrada, bola)){
+            return true;
+        }else{
+            tablero.agregarBola(tuboSalida, bola);
+            return false;
+        }
     }
+    
     public void agregarTercerTubo(){
     }
+    
     public void revisarEstadoJuego(){
     }
 }   
