8081888df5a346bfada5b9a7c13ae4ec51313447
Kenneth Benavides <kbr1312@gmail.com>
2020-09-27 12:50:17 -0600

    Crear Constructor Computadora

--- Tarea_Programada_1/Computadora.java
+++ Tarea_Programada_1/Computadora.java
@@ -1,8 +1,20 @@
 public class Computadora
 {
+    private int cantColores;
+    private int cantTubos;
+    
  
-    public Computadora(){
+    /**
+     * Crea el objeto
+     * 
+     * @param cantColores cantidad de colores diferentes
+     * @param cantTubos cantidad de tubos
+     */
+    public Computadora(int cantColores, int cantTubos){
+        this.cantColores = cantColores;
+        this.cantTubos = cantTubos;
     }
+    
     public void generarDistribucion(){
     }
 }
