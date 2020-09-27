298e36cc8599976a75c725694c0b4c2134690b05
Kenneth Benavides <kbr1312@gmail.com>
2020-09-27 12:40:12 -0600

    Crear Constructor Interfaz

--- Tarea_Programada_1/Interfaz.java
+++ Tarea_Programada_1/Interfaz.java
@@ -6,8 +6,18 @@ public class Interfaz extends JOptionPane
     private String TITULO;
     private ImageIcon IMAGEN;
     
-    public Interfaz(){ 
+    /**
+     * Crea el objeto, el titulo y la imagen que seran usadas en ventanas
+     * 
+     * @param elTitulo El itulo de ventanas.
+     * @param nombreArchivoImagen El nombre del archivo de imagen.
+     */
+    public Interfaz(String elTitulo, String nombreArchivoImagen)
+    {
+        TITULO = elTitulo;
+        IMAGEN = new ImageIcon(this.getClass().getResource(nombreArchivoImagen));
     }
+    
     public void decirMensaje(){
     }
     public void pedirDato(){
